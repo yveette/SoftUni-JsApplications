82712a9227c4be2eef573978918d3e05f0653892
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2021-12-30 18:36:33 +0200

    Added create.js to CarTube

---
+++ 19.OlderExams/CarTube/src/views/create.js
@@ -0,0 +1,69 @@
+import { createCar } from '../api/data.js';
+import { html } from '../lib.js';
+import { getUserData } from '../util.js';
+
+const createTemplate = (onSubmit) => html`
+<section id="create-listing">
+    <div class="container">
+        <form id="create-form" @submit=${onSubmit}>
+            <h1>Create Car Listing</h1>
+            <p>Please fill in this form to create an listing.</p>
+            <hr>
+
+            <p>Car Brand</p>
+            <input type="text" placeholder="Enter Car Brand" name="brand">
+
+            <p>Car Model</p>
+            <input type="text" placeholder="Enter Car Model" name="model">
+
+            <p>Description</p>
+            <input type="text" placeholder="Enter Description" name="description">
+
+            <p>Car Year</p>
+            <input type="number" placeholder="Enter Car Year" name="year">
+
+            <p>Car Image</p>
+            <input type="text" placeholder="Enter Car Image" name="imageUrl">
+
+            <p>Car Price</p>
+            <input type="number" placeholder="Enter Car Price" name="price">
+
+            <hr>
+            <input type="submit" class="registerbtn" value="Create Listing">
+        </form>
+    </div>
+</section>`;
+
+export function createPage(ctx) {
+    ctx.render(createTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        const brand = formData.get('brand');
+        const model = formData.get('model');
+        const description = formData.get('description');
+        const year = Number(formData.get('year'));
+        const imageUrl = formData.get('imageUrl');
+        const price = Number(formData.get('price'));
+
+        if (brand == '' || model == "" || description == '' || year == '' || imageUrl == '' || price == '') {
+            return alert('All fields are required!');
+        }
+
+        if (year < 0 || price < 0) {
+            return alert('The values of year and price must be positive numbers!');
+        }
+
+        await createCar({
+            brand,
+            model,
+            description,
+            year,
+            imageUrl,
+            price
+        });
+        ctx.page.redirect('/catalog');
+    }
+}
