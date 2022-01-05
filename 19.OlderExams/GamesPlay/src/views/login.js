3dbe410c843a6c33a57c32898b42c60cf8fafcb7
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-01-05 23:19:17 +0200

    Added login.js to GamesPlay

---
+++ 19.OlderExams/GamesPlay/src/views/login.js
@@ -0,0 +1,45 @@
+import { login } from '../api/data.js';
+import { html } from '../lib.js';
+
+const loginTemplate = (onSubmit) => html`
+<section id="login-page" class="auth">
+    <form id="login" @submit=${onSubmit}>
+
+        <div class="container">
+            <div class="brand-logo"></div>
+            <h1>Login</h1>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="Sokka@gmail.com">
+
+            <label for="login-pass">Password:</label>
+            <input type="password" id="login-password" name="password">
+            <input type="submit" class="btn submit" value="Login">
+            <p class="field">
+                <span>If you don't have profile click <a href="/register">here</a></span>
+            </p>
+        </div>
+    </form>
+</section>`;
+
+
+export function loginPage(ctx) {
+
+    ctx.render(loginTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+
+        if (email == '' || password == '') {
+            return alert('All fields are required!');
+        }
+
+        await login(email, password);
+        ctx.updateUserNav();
+        event.target.reset();
+        ctx.page.redirect('/');
+    }
+}
