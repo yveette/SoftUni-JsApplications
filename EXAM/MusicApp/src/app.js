c5d589fc0247cb309fe7991b0b7bda6365ca5135
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-01-10 17:21:37 +0200

    Added app.js to MusicApp

---
+++ EXAM/MusicApp/src/app.js
@@ -0,0 +1,52 @@
+import { page, render } from './lib.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+import { logout } from './api/data.js';
+import { getUserData } from './util.js';
+import { homePage } from './views/home.js';
+import { createPage } from './views/create.js';
+import { catalogPage } from './views/catalog.js'; 
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+import { searchPage } from './views/search.js';
+
+const root = document.querySelector('main'); 
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
+
+page(decorateContext);
+page('/', homePage);
+page('/catalog', catalogPage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/create', createPage);
+page('/details/:id', detailsPage);
+page('/edit/:id', editPage);
+page('/search', searchPage);
+
+
+updateUserNav();
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    ctx.updateUserNav = updateUserNav;
+    next();
+}
+
+function updateUserNav() {
+    const userData = getUserData();
+    if (userData) {
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+}
+
+
+function onLogout() {
+    logout();
+    updateUserNav();
+    page.redirect('/');
+}
